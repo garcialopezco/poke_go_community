e988840c7bc30261eb4203d0d2b652e0690ccb93
Carlos García <carlos.garcia@logicalbricks.com>
2018-08-22 08:05:21 -0500

    Add sign up feature spec.

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,6 +2,16 @@ require "rails_helper"
 
 feature "Signing up" do
   scenario "Signing up a new user" do
-    expect(true).to eq true
+    visit "/sign_up"
+    fill_in "Trainer Name", with: "Chaaartz"
+    fill_in "Level", with: "38"
+    fill_in "Phone Number", with: "95112334567"
+    fill_in "Password", with: "MyP@ssword1"
+    fill_in "Password Confirmation", with: "MyP@ssword1"
+
+    click_on "Sign Up"
+
+    expect(page).to have_content "Welcome to the Community Trainer Chaaartz"
+    expect(page).to have_link "Logout"
   end
 end
